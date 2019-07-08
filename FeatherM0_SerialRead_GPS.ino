2085ca5e05f57a821678721a5a33087ba84b8643
matt-bowen <mt.bowen@hotmail.com>
2019-07-08 15:40:13 -0600

    First commit

---
+++ FeatherM0_SerialRead_GPS.ino
@@ -0,0 +1,14 @@
+void setup() {
+  // put your setup code here, to run once:
+  Serial1.begin(4800);
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if (Serial1.available()) {
+    //Serial.print(Serial.write(Serial1.read()));
+    Serial.write(Serial1.read());
+
+  }
+}
